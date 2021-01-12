994790d384f90486002948297043a0706ce52007
Yosef Gamble <yegamble@gmail.com>
2021-01-13 05:09:51 +1300

    add validation to login form.

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,10 +1,11 @@
 import {rules, schema} from "@ioc:Adonis/Core/Validator";
 import User from "App/Models/User";
+import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
 
 
 export default class AuthController {
 
-  public async index({request, response}){
+  public async signup({request, response}){
     const validated = await request.validate({
       schema: schema.create({
         email: schema.string({},[
@@ -12,7 +13,8 @@ export default class AuthController {
           rules.unique({ table: 'users', column: 'email' })
         ]),
         password: schema.string({},[
-          rules.confirmed()
+          rules.confirmed(),
+          rules.minLength(8)
         ]),
       }),
       messages: {
@@ -33,6 +35,26 @@ export default class AuthController {
     await user.save();
 
     return response.redirect('/')
+  }
+
+  public async login({ request }: HttpContextContract){
+      const req = await request.validate({
+        schema:schema.create({
+          email: schema.string({},[
+            rules.email()
+          ]),
+          password: schema.string({},[
+            rules.minLength(8)
+          ])
+        }),
+        messages: {
+          'email.required': 'Email is required',
+          'password.required': 'Password is required',
+          'password.minLength': 'Password must be at least 8 characters',
+        }
+      })
+
+    return 'sucess'
   }
 
 }
--- start/routes.ts
+++ start/routes.ts
@@ -23,7 +23,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.on('/').render('welcome')
 
 Route.on('/login').render('auth/login')
-Route.post('/login','AuthController.index')
+Route.post('/login','AuthController.login')
 
 Route.on('/signup').render('auth/signup')
-Route.post('/signup','AuthController.index')
+Route.post('/signup','AuthController.signup')
