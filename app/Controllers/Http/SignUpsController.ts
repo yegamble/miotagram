e6912e6cf399fac9b4b4269f66814c083766e5e9
Yosef Gamble <yegamble@gmail.com>
2021-01-13 04:53:29 +1300

    signup controller with hashed password.

--- app/Controllers/Http/SignUpsController.ts
+++ app/Controllers/Http/SignUpsController.ts
@@ -1,14 +1,15 @@
 import {rules, schema} from "@ioc:Adonis/Core/Validator";
+import User from "App/Models/User";
+
 
 export default class SignUpsController {
 
-  public async index({request}){
+  public async index({request, response}){
     const validated = await request.validate({
       schema: schema.create({
-        name: schema.string(),
         email: schema.string({},[
           rules.email(),
-          // rules.unique
+          rules.unique({ table: 'users', column: 'email' })
         ]),
         password: schema.string({},[
           rules.confirmed()
@@ -19,12 +20,19 @@ export default class SignUpsController {
         'email.required': 'Email is required to Sign up.',
         'password.required': 'Password is required to Sign up',
         'password.confirmed': 'Passwords do not match',
+        'email.unique': 'Email already exists'
       }
     })
 
-    console.log(validated)
+    const user = new User()
+    user.name = validated.name
+    user.username = 'test'
+    user.email = validated.email
+    user.password = validated.password
 
-    return request.all()
+    await user.save();
+
+    return response.redirect('/')
   }
 
 }
